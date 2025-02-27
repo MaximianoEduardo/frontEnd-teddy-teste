dfffad6eddaedf5eb385e2747d306666ef961d6f
emaximiano <edu.maxi.96@gmail.com>
2025-02-27 13:29:52 -0300

    adiciona componentes de cabeçalho e barra lateral com estilização e testes

---
+++ src/app/ui/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+
+@Component({
+  selector: 'app-header',
+  imports: [SidebarComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
---
+++ src/app/ui/sidebar/sidebar.component.ts
@@ -0,0 +1,48 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent implements OnInit {
+
+  isActive: boolean = false;
+
+  links = [
+    {
+      name: 'Home',
+      path: 'dashboard',
+    },
+    {
+      name: 'Clientes',
+      path: 'dashboard',
+    },
+    {
+      name: 'Produtos',
+      path: 'dashboard',
+    },
+  ]
+
+  constructor(
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.links.map((link) => {
+
+      if(link.name === this.router.url){
+        console.log(link);
+      }
+
+    })
+  }
+
+  open(){
+
+  }
+
+}
