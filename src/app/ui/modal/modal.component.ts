6b74525664152e5b1245bcbe8c4b7dad245aa376
emaximiano <edu.maxi.96@gmail.com>
2025-02-28 14:45:20 -0300

    refatora componente de modal: remove formulário de criação de cliente e integra componente de ação de formulário

--- src/app/ui/modal/modal.component.ts
+++ src/app/ui/modal/modal.component.ts
@@ -1,60 +1,25 @@
-import { Component, computed, Input, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { ModalService } from '../../services/modal/modal.service';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { NgxCurrencyDirective } from "ngx-currency";
-import { Store } from '@ngrx/store';
-import { createClient } from '../../store/clients/create/create.client.actions';
+import { FormActionComponent } from "../form-action/form-action.component";
 
 @Component({
   selector: 'app-modal',
-  imports: [CommonModule, ReactiveFormsModule, NgxCurrencyDirective],
+  imports: [CommonModule, ReactiveFormsModule, FormActionComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
-export class ModalComponent implements OnInit{
+export class ModalComponent {
 
   newClientForm!: FormGroup
 
   constructor(
-    public modalService: ModalService , 
-    private store: Store) 
+    public modalService: ModalService) 
   {}
-
-  @Input()
-  title: string = 'Criar Client';
-
-  ngOnInit(): void {
-    this.newClientForm = new FormGroup({
-      name: new FormControl('', [
-        Validators.required
-      ]),
-      salary: new FormControl(null, [
-        Validators.required
-      ]),
-      companyValuation: new FormControl(null, [
-        Validators.required
-      ]),
-    })
-  }
 
   closeModal() {
     this.modalService.closeModal();
-  }
-
-  // Método para enviar o formulário
-  onSubmit() {
-    if (this.newClientForm.valid) {
-      const newClient = this.newClientForm.value;
-      this.store.dispatch(createClient({ payload: newClient }));
-      this.closeModal();
-    } else {
-      // Handle form errors
-      Object.keys(this.newClientForm.controls).forEach(field => {
-        const control = this.newClientForm.get(field);
-        control?.markAsTouched({ onlySelf: true });
-      });
-    }
   }
 
 }
