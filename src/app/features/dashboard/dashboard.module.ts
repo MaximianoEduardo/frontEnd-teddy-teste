48525db4eecf56b47eb2acd04617e2f5a62a5720
emaximiano <edu.maxi.96@gmail.com>
2025-03-03 12:38:44 -0300

    adicionando guard na estrutura

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './component/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
-
+import { dashboardGuard } from '../../guard/dashboard/dashboard.guard';
 
 const dashboardRoutes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent, canActivate: [dashboardGuard] }
 ];
 
 
